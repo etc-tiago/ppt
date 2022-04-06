f1f6eca0b05e56f89081d0bb98af83485c2318dc
Tiago Souza <33164463+etc-tiago@users.noreply.github.com>
2022-04-06 14:29:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const ppt = require('puppeteer');
 
-(() => {
+(async () => {
   const browser = await puppeteer.launch({
     executablePath: '../../node_modules/puppeteer/.local-chromium/linux-970485/chrome-linux/chrome',
     headless: true,
   });
   this.browser = await browser.newPage();
 
-  const page: puppeteer.Page = await browser.newPage();
+  const page = await browser.newPage();
 
   await browser.close();
 })()
